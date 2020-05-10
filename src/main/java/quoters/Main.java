db6caa543bd52ac9a2e136624a6ee36b6f1c2511
naglec13 <naglec13@gmail.com>
2020-05-10 14:36:36 +0300

    ..

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        // доступ к бину по классу
         context.getBean(TerminatorQuoter.class).sayQuote();
     }
 }
