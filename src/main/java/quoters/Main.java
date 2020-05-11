7a8f338285bfa2493b1a4bb2a1fa51ddc701eef6
naglec13 <naglec13@gmail.com>
2020-05-12 00:48:35 +0300

    ..adding @PostProxy annotation for PostProxy profiling

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -6,13 +6,5 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         // creating IoC container
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-
-        //доступ к бину
-        while (true) {
-            // чтобы не мерцало
-            Thread.sleep(500);
-            // получаем бин по интерфейсу, который он наследует класс (с классом может случиться что угодно)
-            context.getBean(Quoter.class).sayQuote();
-        }
     }
 }
