8906eba7b70db47c063b9ed9cc276f8d6c6b5b1f
naglec13 <naglec13@gmail.com>
2020-05-12 18:48:41 +0300

    ..adding @DeprecatedClass annotation for using BeanFactoryPostProcessor to change the class before BeanFactory

---
+++ src/main/java/quoters/DeprecationHandlerBeanFactoryPostProcessor.java
@@ -0,0 +1,12 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class DeprecationHandlerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+
+    }
+}
