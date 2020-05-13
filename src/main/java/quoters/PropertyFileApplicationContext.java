9327b544391f9d96b22e6dbb6e6b03aeb34baa93
naglec13 <naglec13@gmail.com>
2020-05-13 23:55:42 +0300

    ..writing own contextReader

---
+++ src/main/java/quoters/PropertyFileApplicationContext.java
@@ -0,0 +1,10 @@
+package quoters;
+
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class PropertyFileApplicationContext extends GenericApplicationContext {
+    public PropertyFileApplicationContext(String fileName) {
+        PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(this);
+    }
+}
