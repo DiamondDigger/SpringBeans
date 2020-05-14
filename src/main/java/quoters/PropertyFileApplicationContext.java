4b94587df406f120601ebaf10a44a5738273f791
naglec13 <naglec13@gmail.com>
2020-05-14 15:10:35 +0300

    ..fix missing dot(".") in context.properties  beanName.(class)  which caused error during creation beanDefinition context

--- src/main/java/quoters/PropertyFileApplicationContext.java
+++ src/main/java/quoters/PropertyFileApplicationContext.java
@@ -4,11 +4,18 @@ import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.context.support.GenericApplicationContext;
 
 public class PropertyFileApplicationContext extends GenericApplicationContext {
+
     public PropertyFileApplicationContext(String fileName) {
         //стрекоза - bean definition reader
         PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(this);
         int i = reader.loadBeanDefinitions(fileName);
-        System.out.println("found " + i + "beans");
+        System.out.println("found " + i + " beans");
         refresh();
+    }
+
+
+    public static void main(String[] args) {
+        PropertyFileApplicationContext context = new PropertyFileApplicationContext("context.properties");
+        context.getBean(Quoter.class).sayQuote();
     }
 }
